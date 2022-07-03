d0daae61a8da8b57d7c409840198e43e0d46424e
Simeon Romanov <68448737+archeoss@users.noreply.github.com>
2022-07-03 12:21:19 +0300

    Update settings.py

--- django-learn/settings.py
+++ django-learn/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c$@s2@idbh%o5-@zev((85x)s!!*+v%-)_sy+sy&5c2^nq&x&b'
+SECRET_KEY = '<key>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
